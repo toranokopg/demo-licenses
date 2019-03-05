c23e8f8241d0205f0f79ab6c3163dd71a69b2d4c
toranokopg <44116633+toranokopg@users.noreply.github.com>
2019-03-06 07:57:37 +0900

    First commit

---
+++ app/src/main/java/io/github/toranoko0518/demo/licenses/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.github.toranoko0518.demo.licenses
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("file:///android_asset/licenses.html")
+    }
+}
