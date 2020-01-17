67d71cfaa0925f2314e16fc19052e40ab90de669
toranokopg <44116633+toranokopg@users.noreply.github.com>
2020-01-17 20:04:35 +0900

    Migrate to AndroidX

--- app/src/main/java/io/github/toranoko0518/demo/licenses/MainActivity.kt
+++ app/src/main/java/io/github/toranoko0518/demo/licenses/MainActivity.kt
@@ -1,6 +1,6 @@
 package io.github.toranoko0518.demo.licenses
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
